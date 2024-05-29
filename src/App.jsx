84a5b10044a6b0bedeb7fb1354fc4076f3e5d0b8
klementina1809 <klementina.ua@gmail.com>
2024-05-29 23:18:31 +0200

    cat json

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import { useState } from "react";
+import axios from "axios";
 import "./styles/App.scss";
 
 function App() {
-	const [count, setCount] = useState(0);
+	const [catImg, setCatImg] = useState("");
+
+	const getCat = async () => {
+		const result = await axios("https://cataas.com/cat?json=true");
+		setCatImg(result.data[0]._id);
+		console.log("result", result.data[0]._id);
+	};
 
 	return (
 		<>
@@ -12,8 +19,8 @@ function App() {
 					<option value="2"></option>
 					<option value="3"></option>
 				</select>
-				<img src="https://placehold.co/600x400/" alt="" />
-				<button>Generate my cat</button>
+				<img src={`https://cataas.com/cat/{catImg}`} alt="" />
+				<button onClick={() => getCat()}>Generate my cat</button>
 				<p>
 					Lorem, ipsum dolor sit amet consectetur adipisicing elit.
 					Nemo labore praesentium quasi, porro laudantium repudiandae,
